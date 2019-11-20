bb0c47326c79600f629069bd22060389f5916532
1csanchez <csanchez@aoiths.org>
2019-11-20 23:01:05 +0000

    RPS

--- js/script.js
+++ js/script.js
@@ -8,10 +8,20 @@
 // DOCUMENT READY FUNCTION BELOW
 
 var user="";
-user=$("input").val();
+var random = Math.random();
+var choice="";
 $("#shoot").click(function(){
+    user=$("input").val();
     $("#userChoice").html(user);
 
+    if(random < .33){
+        choice ="scissors";
 
+    }else if(random < .66){
+        choice ="rock";
+
+    }else{
+        choice ="paper";
+    }
+    $("#computerChoice").html(choice);
 });
-
